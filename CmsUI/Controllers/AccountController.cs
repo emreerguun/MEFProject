f04963d802b03c2c5fda67949ed487b8902558df
User <User@DESKTOP-IPNFTKP>
2024-12-19 23:06:59 +0300

    Add project files.

---
+++ CmsUI/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CmsUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
