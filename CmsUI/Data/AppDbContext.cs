150d14a54c59c3e33f5f792b36b679a0993c8c7c
fagcamfe <furkan@DESKTOP-SRPK2HV>
2024-12-24 00:44:41 +0300

    viewmodels

---
+++ CmsUI/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CmsUI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsUI.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions options):base(options) { 
+        
+        }
+    }
+}
--- CmsUI/Program.cs
+++ CmsUI/Program.cs
@@ -1,9 +1,31 @@
+using CmsUI.Data;
+using CmsUI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<AppDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 var app = builder.Build();
+
+builder.Services.AddIdentity<Users, IdentityRole>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.User.RequireUniqueEmail = true;
+    options.SignIn.RequireConfirmedAccount = false;
+    options.SignIn.RequireConfirmedEmail = false;
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+})
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
